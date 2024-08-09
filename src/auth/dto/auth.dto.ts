eb5f2bb05fb953540390314c33e72c1d1d4a99e1
Gr4vity4 <prawared.bw@gmail.com>
2024-08-09 20:16:21 +0700

    Update example auth

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -2,13 +2,13 @@ import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class AuthDto {
-  @ApiProperty({ example: 'user@example.com', description: 'Email' })
+  @ApiProperty({ example: 'admin@example.com', description: 'Email' })
   @IsNotEmpty()
   @IsEmail()
   @IsString()
   readonly email: string;
 
-  @ApiProperty({ example: 'password', description: 'Password' })
+  @ApiProperty({ example: 'Admin@2024!', description: 'Password' })
   @IsNotEmpty()
   @IsString()
   readonly password: string;
