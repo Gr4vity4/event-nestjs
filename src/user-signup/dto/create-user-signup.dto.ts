0fb2dc609cd0dd872a2c6aa20c8f7fe25016a03c
Gr4vity4 <prawared.bw@gmail.com>
2024-08-10 10:46:09 +0700

    Update dto user-signup

--- src/user-signup/dto/create-user-signup.dto.ts
+++ src/user-signup/dto/create-user-signup.dto.ts
@@ -1,23 +1,47 @@
-import { Types } from 'mongoose';
 import { IsString, IsNotEmpty, MinLength, IsMongoId } from 'class-validator';
 import { Transform } from 'class-transformer';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserSignupDto {
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: '66b641e87e6e7778609815f6',
+    description: 'MongoDB ObjectId as a string',
+  })
   @IsNotEmpty()
   @IsMongoId()
-  @Transform(({ value }) => new Types.ObjectId(value))
-  readonly eventId: Types.ObjectId;
+  @Transform(({ value }) => value)
+  readonly eventId: string;
 
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: 'John',
+    description: 'First name of the user',
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   readonly firstName: string;
 
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: 'Doe',
+    description: 'Last name of the user',
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(3)
   readonly lastName: string;
 
+  @ApiProperty({
+    type: String,
+    required: true,
+    example: '08123456789',
+    description: 'Phone number of the user',
+  })
   @IsNotEmpty()
   @IsString()
   @MinLength(10)
