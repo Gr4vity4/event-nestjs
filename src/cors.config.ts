f29e57b053fcbd0db7829fb5a7b3f6163002258b
Gr4vity4 <prawared.bw@gmail.com>
2024-08-11 23:26:28 +0700

    Update cors

--- src/cors.config.ts
+++ src/cors.config.ts
@@ -1,4 +1,7 @@
-export const allowedOrigins = ['http://localhost:3001'];
+export const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+];
 
 export const corsOptions = {
   origin: (origin, callback) => {
